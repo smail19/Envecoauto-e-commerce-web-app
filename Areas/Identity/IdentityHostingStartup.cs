92af25254acae0c36a6e50365de0a8e1096bbd42
smail <alyaminsmail@gmail.com>
2022-02-13 16:14:17 +0100

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Envecoauto.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+[assembly: HostingStartup(typeof(Envecoauto.Areas.Identity.IdentityHostingStartup))]
+namespace Envecoauto.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options =>
+            { options.SignIn.RequireConfirmedAccount = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+
+
+                })
+             
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
